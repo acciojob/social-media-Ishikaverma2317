0a2fc7e18ff5babb170f3215931fb0a74d216297
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-29 16:49:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import PostsList from "./PostsList";
 import UsersList from "./UsersList";
 import UserPosts from "./UserPosts";
 import Notifications from "./Notifications";
+import AddPostForm from "./AddPostForm";
 import EditPost from "./EditPost";
 
 function App() {
   return (
-    <Router>
+    <div className="App">
       <h1>GenZ</h1>
+      <Navbar />
 
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<PostsList />} />
-          <Route path="/users" element={<UsersList />} />
-          <Route path="/users/:userId" element={<UserPosts />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/posts/:postId" element={<EditPost />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/users/:userId" element={<UserPosts />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/addPost" element={<AddPostForm />} />
+        <Route path="/posts/:postId" element={<EditPost />} />
+      </Routes>
+    </div>
   );
 }
 
