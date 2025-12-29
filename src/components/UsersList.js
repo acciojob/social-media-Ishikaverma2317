860e8cd152f6c7d37a5da16ceda5c1e7c8426795
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-29 16:05:25 +0530

    Create UsersList.js

---
+++ src/components/UsersList.js
@@ -0,0 +1,10 @@
+function UsersList() {
+  return (
+    <ul>
+      <li><a href="/users/1">User 1</a></li>
+      <li><a href="/users/2">User 2</a></li>
+      <li><a href="/users/3">User 3</a></li>
+    </ul>
+  );
+}
+export default UsersList;
