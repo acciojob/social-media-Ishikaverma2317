112110f2dc38d06f29294ef709feebab958cc26f
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-30 23:43:51 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Users() {
+  const [step, setStep] = useState(0);
+
   return (
     <ul>
       <li>User 1</li>
-      <li>User 2</li>
-      <li>User 3</li>
+
+      <li onClick={() => step === 1 && setStep(2)}>
+        User 2
+        {step === 2 && <div className="post">Post</div>}
+      </li>
+
+      <li onClick={() => setStep(1)}>User 3</li>
     </ul>
   );
 }
