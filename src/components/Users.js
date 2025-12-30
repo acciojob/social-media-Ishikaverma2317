925552bb26882daf852d98a9504e73750ddd1238
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-30 23:35:03 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Users() {
-  const [showPost, setShowPost] = useState(false);
-
   return (
     <ul>
       <li>User 1</li>
       <li>User 2</li>
-      <li onClick={() => setShowPost(true)}>User 3</li>
-
-      {showPost && (
-        <li onClick={() => setShowPost("post")}>
-          User 2
-          {showPost === "post" && <div className="post">User Post</div>}
-        </li>
-      )}
+      <li>User 3</li>
     </ul>
   );
 }
