071c0c458d8cb8b2bfdd88db92b7958172707441
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-30 23:44:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,10 @@ function App() {
     <div className="App">
       <h1>GenZ</h1>
 
-      <nav>
-        <a href="/">Posts</a>
-        <a href="/users">Users</a>
-        <a href="/notifications">Notifications</a>
-      </nav>
+      {/* MUST ALWAYS EXIST */}
+      <a href="/">Posts</a>
+      <a href="/users">Users</a>
+      <a href="/notifications">Notifications</a>
 
       <Switch>
         <Route exact path="/" component={PostsList} />
