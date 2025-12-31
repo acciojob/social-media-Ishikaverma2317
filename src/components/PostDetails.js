2201a03580311bbad1a686474a91654532e32ff1
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-31 20:20:16 +0530

    Update PostDetails.js

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 
-function PostDetails() {
-  return (
-    <div className="post">
-      <input id="postTitle" />
-      <textarea id="postContent"></textarea>
+class PostDetails extends Component {
+  state = {
+    editing: false,
+    title: "Hello",
+    content: "First post"
+  };
 
-      <button className="button">Edit</button>
-      <button>Save</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="post">
+        {this.state.editing ? (
+          <>
+            <input
+              id="postTitle"
+              value={this.state.title}
+              onChange={(e) => this.setState({ title: e.target.value })}
+            />
+            <textarea
+              id="postContent"
+              value={this.state.content}
+              onChange={(e) => this.setState({ content: e.target.value })}
+            />
+            <button onClick={() => this.setState({ editing: false })}>
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <h3>{this.state.title}</h3>
+            <p>{this.state.content}</p>
+            <button
+              className="button"
+              onClick={() => this.setState({ editing: true })}
+            >
+              Edit
+            </button>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default PostDetails;
- 
