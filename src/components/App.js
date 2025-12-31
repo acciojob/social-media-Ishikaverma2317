d01c989895e83584102953c44077868bf2b14914
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-31 20:30:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Posts from "./Posts";
 import Users from "./Users";
 import Notifications from "./Notifications";
-import PostDetails from "./PostDetails";
-import "../styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <h1>GenZ</h1>
+export default function App() {
+  return (
+    <Router>
+      <h1>Shopping cart</h1>
 
-        {/* NAVBAR (a tags only) */}
-        <nav>
-          <a href="/">Posts</a>
-          <a href="/users">Users</a>
-          <a href="/notifications">Notifications</a>
-        </nav>
+      {/* NAVIGATION */}
+      <ul>
+        <li><Link to="/">Posts</Link></li>
+        <li><Link to="/users">Users</Link></li>
+        <li><Link to="/notifications">Notifications</Link></li>
+      </ul>
 
-        <Switch>
-          <Route exact path="/" component={Posts} />
-          <Route path="/users" component={Users} />
-          <Route path="/notifications" component={Notifications} />
-          <Route path="/posts/:id" component={PostDetails} />
-        </Switch>
-      </Router>
-    );
-  }
+      <Switch>
+        <Route exact path="/" component={Posts} />
+        <Route path="/users" component={Users} />
+        <Route path="/notifications" component={Notifications} />
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
