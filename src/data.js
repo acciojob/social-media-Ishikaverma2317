9ae044a555519fe153c43ca52b7a30a9f2b19b6d
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-31 21:33:40 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const usersData = [
+  { id: 1, name: "Ishu" },
+  { id: 2, name: "Ishika" },
+  { id: 3, name: "Koko" }
+];
+
+export const postsData = [
+  { id: 1, title: "Hello World", author: "Ishu", content: "This is my first post" },
+  { id: 2, title: "React Rocks", author: "Ishika", content: "React is awesome!" },
+  { id: 3, title: "GenZ News", author: "Koko", content: "Stay updated with GenZ" }
+];
