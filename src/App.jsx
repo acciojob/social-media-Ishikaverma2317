744c873e043038f21c1b60963218bfb97a2fcc0f
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-31 21:35:08 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import Notifications from './pages/Notifications';
+import PostDetails from './pages/PostDetails';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>GenZ</h1>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
