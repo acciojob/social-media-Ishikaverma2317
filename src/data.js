c1b77bf76edbc0b014672f7f9b433ef553a14c12
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-01 11:26:29 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
-export const usersData = [
-  { id: 1, name: "Ishu" },
-  { id: 2, name: "Ishika" },
-  { id: 3, name: "Koko" }
+export const users = [
+  { id: "1", name: "Leanne Graham" },
+  { id: "2", name: "Ervin Howell" },
+  { id: "3", name: "Clementine Bauch" }
 ];
 
-export const postsData = [
-  { id: 1, title: "Hello World", author: "Ishu", content: "This is my first post" },
-  { id: 2, title: "React Rocks", author: "Ishika", content: "React is awesome!" },
-  { id: 3, title: "GenZ News", author: "Koko", content: "Stay updated with GenZ" }
+export const posts = [
+  {
+    id: "1",
+    title: "Hello World",
+    content: "First post content",
+    user: "Leanne Graham",
+    reactions: { like: 0, heart: 0, rocket: 0, coffee: 0 }
+  }
 ];
