6da663af75795d3df8473cfa8ef6a1728ea4d9f2
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-01 11:21:23 +0530

    Create Notifications.js

---
+++ src/components/Notifications.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function Notifications() {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button className="button" onClick={() => setShow(true)}>
+        Refresh Notifications
+      </button>
+
+      <section className="notificationsList">
+        {show && <div>New notification</div>}
+      </section>
+    </>
+  );
+}
