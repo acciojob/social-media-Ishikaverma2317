033c65d6a74e456721b2bd05c10aaa336ede79b8
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-01 11:19:57 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="/">Posts</a>
+      <a href="/users">Users</a>
+      <a href="/notifications">Notifications</a>
+    </nav>
+  );
+}
