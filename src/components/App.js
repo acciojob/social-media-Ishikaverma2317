3340519ec3177d096f2eb64ca59e0b88d0652085
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-01 11:19:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import PostsList from "./PostsList";
+import PostDetail from "./PostDetail";
+import Users from "./Users";
+import Notifications from "./Notifications";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <h1>GenZ</h1>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<PostsList />} />
+          <Route path="/posts/:postId" element={<PostDetail />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
