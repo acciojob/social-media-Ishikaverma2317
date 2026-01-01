75af5338c182dd9ffb14f6f6e8df4a41f8f54689
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-01 13:19:02 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { users, posts } from "../data";
 
 export default function Users() {
   return (
     <ul>
-      {users.map(u => (
-        <li key={u.id}>
-          <ul>
-            {posts
-              .filter(p => p.user === u.name)
-              .map(p => (
-                <li key={p.id}>
-                  <div className="post">{p.title}</div>
-                </li>
-              ))}
-          </ul>
-        </li>
-      ))}
+      <li>User 1</li>
+      <li>User 2</li>
+      <li>User 3</li>
     </ul>
   );
 }
