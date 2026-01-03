fc0723adbc3c1a1f924e03a39d647e5111f85728
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-03 14:03:16 +0530

    Update PostDetail.js

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,38 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useState } from "react";
 
 function PostDetail() {
-  const { postId } = useParams();
-  const [edit, setEdit] = useState(false);
-  const [title, setTitle] = useState("Post Title");
-  const [content, setContent] = useState("Post Content");
-
   return (
-    <div className="post">
-      {edit ? (
-        <>
-          <input
-            id="postTitle"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-          <textarea
-            id="postContent"
-            value={content}
-            onChange={e => setContent(e.target.value)}
-          />
-          <button onClick={() => setEdit(false)}>Save</button>
-        </>
-      ) : (
-        <>
-          <h2>{title}</h2>
-          <p>{content}</p>
-          <button className="button" onClick={() => setEdit(true)}>
-            Edit
-          </button>
-        </>
-      )}
+    <div>
+      <input id="editTitle" />
+      <textarea id="editContent"></textarea>
+      <button>Save</button>
     </div>
   );
 }
