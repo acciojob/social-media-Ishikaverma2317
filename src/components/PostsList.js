fb19495c1abe80f69b28247c8e006370506543fc
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-03 13:36:04 +0530

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,24 +1,14 @@
 import React from "react";
+import Posts from "./Posts";
 
 function PostsList() {
   return (
     <section className="posts-list">
-      {/* FIRST CHILD */}
       <h2>Posts</h2>
 
       {/* SECOND CHILD */}
       <div>
-        <article>
-          <a href="/posts/1" className="button">
-            View
-          </a>
-
-          <button>👍</button>
-          <button>❤️</button>
-          <button>🚀</button>
-          <button>👀</button>
-          <button>0</button>
-        </article>
+        <Posts />
       </div>
     </section>
   );
