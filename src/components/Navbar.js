ec2608c7ef2fe2ce00de4800cd8862900b1d8cc6
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-05 12:34:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="nav">
-      <section>
-        <h1>Shopping cart</h1>
-        <div className="navLinks">
-          <Link to="/">Posts</Link>
-          <Link to="/users">Users</Link>
-          <Link to="/notifications">Notifications</Link>
-          <Link to="/create-post">Create Post</Link>
-        </div>
-      </section>
+    <nav>
+      <a href="/">Posts</a>
+      <a href="/users">Users</a>
+      <a href="/notifications">Notifications</a>
     </nav>
   );
 }
