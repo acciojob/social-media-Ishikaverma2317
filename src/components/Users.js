661c9b329fd695c3064d52407eb887725e0b059d
Ishika Verma <225302307+Ishikaverma2317@users.noreply.github.com>
2026-01-05 12:34:41 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,20 +1,7 @@
 import React from "react";
 
 function Users() {
-  return (
-    <ul>
-      <li>
-        <a href="/users/1">User 1</a>
-      </li>
-      <li>
-        <a href="/users/2">User 2</a>
-      </li>
-      <li>
-        <a href="/users/3">User 3</a>
-      </li>
-    </ul>
-  );
+  return <h1>Users</h1>;
 }
 
 export default Users;
- 
